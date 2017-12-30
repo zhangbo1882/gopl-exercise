d6080fbf86a292ec11ae07bb79b399d4c6844b5f
Aaron (Bo) Zhang <azhang@sonicwall.com>
2017-12-30 17:24:47 +0800

    ch3(exe3.13)

---
+++ ch3/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const (
+	KB = 1000
+	MB = KB * KB
+	GB = MB * KB
+	TB = GB * KB
+	PB = TB * KB
+	EB = PB * KB
+	ZB = EB * KB
+	YM = TB * KB
+)
+
+func main() {
+	fmt.Println("exe3.13")
+}
